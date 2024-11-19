4ad173b6c6919dc6539a9bdfb31991d398090685
kirsaxg <dooe_x@vk.com>
2024-11-19 20:40:52 +0300

    1000-7

--- 1.c
+++ 1.c
@@ -1,25 +1,25 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿#include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-char* VMP(  unsigned int num, int r)
+char* VMP( unsigned int num, int r)
 {
-	if ( r < 1 || r > 5)
+	if ( r < 1 || r > 5) 
 	{
-		puts("Error. Pishi pravilno chislo!");
+		puts("Error");
 		return NULL;
 	}
 
-	if (num == 0 )
+	if ( num == NULL)
 	{
 		char* res = (char*)malloc(2);
 		if (res == NULL) return NULL;
-		strcpy(res, "0");
+		strcpy(res, "\0");
 		return res;
 	}
 
 	unsigned int base = 1U << r;
+
 	int len = 0;
 	unsigned int temp = num;
 
@@ -28,43 +28,44 @@ char* VMP(  unsigned int num, int r)
 		len++;
 		temp >>= r;
 	}
-
+	
 	char* result = (char*)malloc(len + 1);
-	if (result == NULL) return NULL;
+	if (result == 0) return NULL;
 	result[len] = '\0';
 
 	char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	for (int i = len - 1; i >=0; i--)
+	for (int i = len -1; i>=0; i--)
 	{
 		unsigned int remainder = num & (base - 1);
-
 		result[i] = digits[remainder];
 		num >>= r;
 	}
-
 	return result;
-
 }
+
 int main()
 {
-	int unsigned num;
+	unsigned int num;
 	int r;
+	
+	puts("Enter num:");
+	scanf("%u", &num);
 
-	puts("Enter num");
-	scanf("%d", &num);
-
-	puts("Enter r (1-5)");
+	puts("Enter r:");
 	scanf("%d", &r);
 
 	char* result = VMP(num, r);
 	
 	if (result)
 	{
-		printf("Final: %s\n", result);
+		printf("Final = %s\n", result);
 		free(result);
+	} 
+	else
+	{
+		puts("Error");
 	}
-	else { puts("Error"); }
 
 	return 0;
 }
